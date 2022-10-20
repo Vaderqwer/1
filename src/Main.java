d3311223e8dd6e91d223702b61b5f3aca3d3c989
INDUS2RY <den.prohorov@internet.ru>
2022-10-20 22:26:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args) {
+        double a = ((101+0)/3);
+        System.out.println(a);
+        double b = (3*(10^-6))*0000000.1;
+        System.out.println(b);
+        boolean c = true&&true;
+        System.out.println(c);
+        boolean d = false&&true;
+        System.out.println(d);
+        boolean e = (false&&false)|(true&&true);
+        System.out.println(e);
+    }
+}
